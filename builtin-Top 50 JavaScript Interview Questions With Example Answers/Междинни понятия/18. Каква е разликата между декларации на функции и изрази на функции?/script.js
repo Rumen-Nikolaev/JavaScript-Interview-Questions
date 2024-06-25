bd476d84aecd6de76676d06b5fe51c967f63c532
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-06-26 00:33:42 +0300

    Update script.js

--- builtin-Top 50 JavaScript Interview Questions With Example Answers/Междинни понятия/18. Каква е разликата между декларации на функции и изрази на функции?/script.js
+++ builtin-Top 50 JavaScript Interview Questions With Example Answers/Междинни понятия/18. Каква е разликата между декларации на функции и изрази на функции?/script.js
@@ -1,1 +1,12 @@
+// Function declaration
+function multiply(a, b) {
+  return a * b;
+}
 
+// Function expression
+const add = function(a, b) {
+  return a + b;
+};
+
+console.log(multiply(2, 3)); // Output: 6
+console.log(add(2, 3)); // Output: 5
