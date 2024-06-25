8cb918d441520a254060602b35ec0abcf742cf11
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-06-25 23:13:34 +0300

    Create script.js

---
+++ builtin-Top 50 JavaScript Interview Questions With Example Answers/Основи на JavaScript/11. Каква е разликата между “ let” , “ const” и “ var” ?/script.js
@@ -0,0 +1,11 @@
+let x = 5;
+x = 10;
+console.log(x); // Output: 10
+
+const y = 5;
+y = 10; // Error: Assignment to constant variable
+console.log(y);
+
+var z = 5;
+var z = 10;
+console.log(z); // Output: 10
