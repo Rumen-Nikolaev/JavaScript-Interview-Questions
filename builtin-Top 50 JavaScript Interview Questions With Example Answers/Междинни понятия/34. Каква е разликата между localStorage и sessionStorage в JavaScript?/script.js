9604e72ead83b944df1521ff62be1e8f49a27670
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-06-26 18:04:19 +0300

    Create script.js

---
+++ builtin-Top 50 JavaScript Interview Questions With Example Answers/Междинни понятия/34. Каква е разликата между localStorage и sessionStorage в JavaScript?/script.js
@@ -0,0 +1,5 @@
+localStorage.setItem("name", "John");
+console.log(localStorage.getItem("name")); // Output: John
+
+sessionStorage.setItem("name", "John");
+console.log(sessionStorage.getItem("name")); // Output John
