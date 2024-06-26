6a0f6b170f92953cc5931b55ea3eccb543eec2a9
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-06-26 13:04:44 +0300

    Create script.js

---
+++ builtin-Top 50 JavaScript Interview Questions With Example Answers/Междинни понятия/26. Каква е разликата между синхронното и асинхронното програмиране?/script.js
@@ -0,0 +1,9 @@
+console.log("Statement 1");
+console.log("Statement 2");
+console.log("Statement 3");
+
+console.log("Statement 1");
+setTimeout(function() {
+  console.log("Statement 2");
+}, 2000);
+console.log("Statement 3");
