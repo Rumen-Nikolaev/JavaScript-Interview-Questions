8606b2fcaa1bd7113ae3ece832dbfe4377080c26
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-06-26 14:43:08 +0300

    Create script.js

---
+++ builtin-Top 50 JavaScript Interview Questions With Example Answers/Междинни понятия/28. Обяснете концепцията за бълбукане на събития в JavaScript./script.js
@@ -0,0 +1,9 @@
+document.getElementById("child").addEventListener("click",
+function() {
+  console.log("Child clicked");
+});
+
+document.getElementById("parent").addEventListener("click", 
+function() {
+  console.log("Parent clicked");
+});
