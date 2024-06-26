9b5af637a2de9dd66462b83780a1a88466f3720a
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-06-26 15:16:21 +0300

    Create script.js

---
+++ builtin-Top 50 JavaScript Interview Questions With Example Answers/Междинни понятия/30. Каква е разликата между querySelector и getElementById ?/script.js
@@ -0,0 +1,5 @@
+const element1 = document.querySelector("#myDiv");
+console.log(element1.textContent); // Output: Hello, World!
+
+const element2 = document.getElementById("myDiv");
+console.log(element2.textContent); // Output: Hello, World!
